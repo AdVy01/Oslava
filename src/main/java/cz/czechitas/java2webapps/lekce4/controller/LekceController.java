9a1fa6d934c079ec9e55d539cb11b3ed731f59b1
adamVysinsky <adam.vysinsky@email.cz>
2021-04-26 21:07:53 +0200

    Lekce 4

--- src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/controller/LekceController.java
@@ -1,8 +1,16 @@
 package cz.czechitas.java2webapps.lekce4.controller;
 
+import cz.czechitas.java2webapps.lekce4.entity.Lekce;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  * Kontroler obsluhující sekci lekcí.
@@ -11,11 +19,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/lekce")
 public class LekceController {
 
+  private final List<Lekce> lekce;
+
+  public LekceController() {
+    lekce = Arrays.asList(
+            new Lekce("Lekce 1", "21. dubna 2021", "18:00", "20:30", "V mechu"),
+            new Lekce("Lekce 2", "28. dubna 2021", "18:00", "20:30", "V kapradí"),
+            new Lekce("Lekce 3", "5. května 2021", "18:00", "20:30", "bude upřesněno")
+    );
+  }
+
   /**
    * Metoda kontroleru obsluhující adresu {@code /lekce/}. Používá šablonu {@code /templates/lekce/index.html}.
    */
   @GetMapping("/")
-  public String lekce() {
-    return "lekce/index";
+  public ModelAndView lekce() {
+    ModelAndView modelAndView = new ModelAndView("lekce/index");
+    modelAndView.addObject("lekce", lekce);
+    return modelAndView;
   }
 }
--- src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
+++ src/main/java/cz/czechitas/java2webapps/lekce4/entity/Lekce.java
@@ -7,9 +7,61 @@ import java.time.LocalTime;
  * Informace o jedné lekci.
  */
 public class Lekce {
+  
   private String nazev;
-  private LocalDate datum;
-  private LocalTime casOd;
-  private LocalTime casDo;
+  private String datum;
+  private String casOd;
+  private String casDo;
   private String misto;
+
+  public Lekce() {
+  }
+
+  public Lekce(String nazev, String datum, String casOd, String casDo, String misto) {
+    this.nazev = nazev;
+    this.datum = datum;
+    this.casOd = casOd;
+    this.casDo = casDo;
+    this.misto = misto;
+  }
+
+  public String getNazev() {
+    return nazev;
+  }
+
+  public void setNazev(String nazev) {
+    this.nazev = nazev;
+  }
+
+  public String getDatum() {
+    return datum;
+  }
+
+  public void setDatum(String datum) {
+    this.datum = datum;
+  }
+
+  public String getCasOd() {
+    return casOd;
+  }
+
+  public void setCasOd(String casOd) {
+    this.casOd = casOd;
+  }
+
+  public String getCasDo() {
+    return casDo;
+  }
+
+  public void setCasDo(String casDo) {
+    this.casDo = casDo;
+  }
+
+  public String getMisto() {
+    return misto;
+  }
+
+  public void setMisto(String misto) {
+    this.misto = misto;
+  }
 }
